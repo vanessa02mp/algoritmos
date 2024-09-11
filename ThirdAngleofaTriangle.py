d4cf861e753108920ffb8b1df5576ddf4132bee3
vanesa02mp <vanemenesa@gmail.com>
2024-09-10 22:57:20 -0600

    se agrega descripcion del problema a resolver

--- ThirdAngleofaTriangle.py
+++ ThirdAngleofaTriangle.py
@@ -1,3 +1,6 @@
+#Problema An NBA game runs 48 minutes (Four 12 minute quarters). Players do not typically play the full game, subbing in and out as necessary. Your job is to extrapolate a player's points per game if they played the full 48 minutes.
+
+#Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game) and returns a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth. Return 0 if 0.
 #Este programa encuentra el angulo de un triangulo apartor de los otros
 def find_third_angle(angle1, angle2):
     # Calcular el tercer ángulo
